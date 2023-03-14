be6f430e95caa485da5b91b03ab1dec56d1684ea
judenm <124482803+judenm@users.noreply.github.com>
2023-03-13 21:08:21 -0400

    Add files via upload

---
+++ sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.leszko.calculator;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+* Adding two integers to get the sum
+*/
+@Service
+public class Calculator {
+        String umlNUMBER = "3k";
+        @Cacheable("sum")
+        public int sum(int a, int b) {
+                return a + b;
+        }
+}
